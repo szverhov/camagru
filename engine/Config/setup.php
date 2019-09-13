ba084f182e4b23a718dd045ea2557b1cdecac34d
пупкин Славик <szverhov@gmail.com>
2019-09-13 14:12:03 +0300

    asd

--- engine/Config/setup.php
+++ engine/Config/setup.php
@@ -1,4 +1,23 @@
 <?php
 	$config = require_once './database.php';
-	print_r($config);
-	exec("mysql -u {$config['username']} {$config['dbname']} < ./camagru_db.sql");
+	$conn = mysqli_connect($config['host'], $config['username'], $config['password']);
+	$conn->query ("CREATE DATABASE {$config['dbname']}");
+	$conn->select_db($config['dbname']);
+	$templine = '';
+	$lines = file('./camagru_db.sql');
+	foreach ($lines as $line)
+	{
+		if (substr($line, 0, 2) == '--' || $line == '')
+		    continue;
+		$templine .= $line;
+		if (substr(trim($line), -1, 1) == ';')
+		{
+		    if (!$stmt = $conn->query(str_replace(';', '', $templine)))
+		    {
+		    	print_r(str_replace(';', '', $templine));
+		    	continue;
+		    }
+		    $templine = '';
+		}
+	}
+	echo "Tables imported successfully";
